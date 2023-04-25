c919d6b01cf3987e4fef0ee51f4a8fb13510e8f7
Sai Chandan Mohanty <76147965+scarface68@users.noreply.github.com>
2023-04-25 09:13:36 +0530

    clearing database and reinserting

--- user.model.js
+++ user.model.js
@@ -1,23 +1,6 @@
 const mongoose = require("mongoose");
 const data = require("./sample_data").data;
 
-// sample data
-const sampleData = [
-  {
-    id: 1,
-    first_name: "Inglis",
-    last_name: "McMurty",
-    email: "imcmurty0@youku.com",
-    gender: "Male",
-    income: "$1.36",
-    city: "Las Flores",
-    car: "BMW",
-    quote: "optimize web-enabled relationships",
-    phone_price: "22236",
-  },
-  // more objects here...
-];
-
 const userSchema = new mongoose.Schema({
   id: Number,
   first_name: String,
@@ -33,11 +16,9 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", userSchema);
 
-if (User.exists({})) {
-  User.deleteMany({})
-    .then(() => console.log("Cleared Database"))
-    .catch((err) => console.log("Failed to delete", err));
-}
+User.deleteMany({})
+  .then(() => console.log("Cleared Database"))
+  .catch((err) => console.log("Failed to delete", err));
 
 User.insertMany(data)
   .then(() => console.log("Sample data inserted"))
